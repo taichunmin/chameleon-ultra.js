73cb4757214350b7471611ce765c2f9862b91a06
taichunmin <taichunmin@gmail.com>
2023-09-22 10:09:12 +0800

    livereload debounce

--- src/ResponseDecoder.ts
+++ src/ResponseDecoder.ts
@@ -13,7 +13,10 @@ export class SlotInfo {
 
   static fromCmd1019 (buf: Buffer): SlotInfo[] {
     if (!Buffer.isBuffer(buf) || buf.length < 8) throw new TypeError('buf should be a Buffer with length 8')
-    return _.times(8, i => new SlotInfo(buf[i * 2], buf[i * 2 + 1]))
+    return _.times(8, i => new SlotInfo(
+      buf[i * 2], // hfTagType
+      buf[i * 2 + 1], // lfTagType
+    ))
   }
 }
 
@@ -22,13 +25,15 @@ export class BatteryInfo {
   level: number
 
   constructor (voltage: number, level: number) {
-    this.voltage = voltage
-    this.level = level
+    ;[this.voltage, this.level] = [voltage, level]
   }
 
   static fromCmd1025 (buf: Buffer): BatteryInfo {
     if (!Buffer.isBuffer(buf) || buf.length !== 3) throw new TypeError('buf should be a Buffer with length 3')
-    return new BatteryInfo(buf.readUInt16BE(0), buf[2])
+    return new BatteryInfo(
+      buf.readUInt16BE(0), // voltage
+      buf[2], // level
+    )
   }
 }
 
@@ -40,7 +45,14 @@ export class DeviceSettings {
   blePairingMode: boolean
   blePairingKey: string
 
-  constructor (version: number, animation: AnimationMode, buttonPressAction: ButtonAction[], buttonLongPressAction: ButtonAction[], blePairingMode: boolean, blePairingKey: string) {
+  constructor (
+    version: number,
+    animation: AnimationMode,
+    buttonPressAction: ButtonAction[],
+    buttonLongPressAction: ButtonAction[],
+    blePairingMode: boolean,
+    blePairingKey: string
+  ) {
     this.version = version
     this.animation = animation
     this.buttonPressAction = buttonPressAction
@@ -52,12 +64,12 @@ export class DeviceSettings {
   static fromCmd1034 (buf: Buffer): DeviceSettings {
     if (!Buffer.isBuffer(buf) || buf.length !== 12) throw new TypeError('buf should be a Buffer with length 12')
     return new DeviceSettings(
-      buf[0],
-      buf[1],
-      [...buf.subarray(2, 4).values()],
-      [...buf.subarray(4, 6).values()],
-      buf[6] === 1,
-      buf.subarray(7, 13).toString('utf8'),
+      buf[0], // version
+      buf[1], // animation
+      [...buf.subarray(2, 4).values()], // buttonPressAction
+      [...buf.subarray(4, 6).values()], // buttonLongPressAction
+      buf[6] === 1, // blePairingMode
+      buf.subarray(7, 13).toString('utf8'), // blePairingKey
     )
   }
 }
@@ -72,28 +84,24 @@ export class Hf14aAntiColl {
   ats: Buffer
 
   constructor (uid: Buffer, atqa: Buffer, sak: Buffer, ats: Buffer = new Buffer()) {
-    if (!Buffer.isBuffer(uid) || uid.length !== 4) throw new TypeError('uid should be a Buffer with length 4')
-    if (!Buffer.isBuffer(atqa) || atqa.length !== 2) throw new TypeError('atqa should be a Buffer with length 2')
-    if (!Buffer.isBuffer(sak) || sak.length !== 1) throw new TypeError('sak should be a Buffer with length 1')
-    if (!Buffer.isBuffer(ats)) throw new TypeError('ats should be a Buffer')
     ;[this.uid, this.atqa, this.sak, this.ats] = [uid, atqa, sak, ats]
   }
 
   static fromCmd2000 (buf: Buffer): Hf14aAntiColl {
     if (!Buffer.isBuffer(buf) || buf.length !== 15) throw new TypeError('buf should be a Buffer with length 15')
     return new Hf14aAntiColl(
-      buf.subarray(0, buf[10]),
-      buf.subarray(13, 15),
-      buf.subarray(12, 13),
+      buf.subarray(0, buf[10]), // uid
+      buf.subarray(13, 15), // atqa
+      buf.subarray(12, 13), // sak
     )
   }
 
   static fromCmd4018 (buf: Buffer): Hf14aAntiColl {
     if (!Buffer.isBuffer(buf) || buf.length !== 16) throw new TypeError('buf should be a Buffer with length 16')
     return new Hf14aAntiColl(
-      buf.subarray(0, buf[10]),
-      buf.subarray(13, 15),
-      buf.subarray(12, 13),
+      buf.subarray(0, buf[10]), // uid
+      buf.subarray(13, 15), // atqa
+      buf.subarray(12, 13), // sak
     )
   }
 }
@@ -107,7 +115,15 @@ export class Mf1DarksideArgs {
   nr?: Buffer
   ar?: Buffer
 
-  constructor (status: DarksideStatus, uid?: Buffer, nt1?: Buffer, par?: Buffer, ks1?: Buffer, nr?: Buffer, ar?: Buffer) {
+  constructor (
+    status: DarksideStatus,
+    uid?: Buffer,
+    nt1?: Buffer,
+    par?: Buffer,
+    ks1?: Buffer,
+    nr?: Buffer,
+    ar?: Buffer
+  ) {
     this.status = status
     this.uid = uid
     this.nt1 = nt1
@@ -120,13 +136,13 @@ export class Mf1DarksideArgs {
   static fromCmd2004 (buf: Buffer): Mf1DarksideArgs {
     if (!Buffer.isBuffer(buf) || buf.length !== 32) throw new TypeError('buf should be a Buffer with length 32')
     return new Mf1DarksideArgs(
-      DarksideStatus.OK,
-      buf.subarray(0, 4),
-      buf.subarray(4, 8),
-      buf.subarray(8, 16),
-      buf.subarray(16, 24),
-      buf.subarray(24, 28),
-      buf.subarray(28, 32),
+      DarksideStatus.OK, // status
+      buf.subarray(0, 4), // uid
+      buf.subarray(4, 8), // nt1
+      buf.subarray(8, 16), // par
+      buf.subarray(16, 24), // ks1
+      buf.subarray(24, 28), // nr
+      buf.subarray(28, 32), // ar
     )
   }
 }
@@ -136,15 +152,14 @@ export class Mf1NtDistanceArgs {
   distance: Buffer
 
   constructor (uid: Buffer, distance: Buffer) {
-    this.uid = uid
-    this.distance = distance
+    ;[this.uid, this.distance] = [uid, distance]
   }
 
   static fromCmd2005 (buf: Buffer): Mf1NtDistanceArgs {
     if (!Buffer.isBuffer(buf) || buf.length !== 8) throw new TypeError('buf should be a Buffer with length 8')
     return new Mf1NtDistanceArgs(
-      buf.subarray(0, 4),
-      buf.subarray(4, 8),
+      buf.subarray(0, 4), // uid
+      buf.subarray(4, 8), // distance
     )
   }
 }
@@ -156,14 +171,16 @@ export class Mf1NestedArgs {
   par: number // The puppet test of the communication process of nested verification encryption, only the 'low 3 digits', that is, the right 3
 
   constructor (nt1: Buffer, nt2: Buffer, par: number) {
-    this.nt1 = nt1
-    this.nt2 = nt2
-    this.par = par
+    ;[this.nt1, this.nt2, this.par] = [nt1, nt2, par]
   }
 
   static fromCmd2006 (buf: Buffer): Mf1NestedArgs[] {
     if (!Buffer.isBuffer(buf)) throw new TypeError('buf should be a Buffer')
-    return _.map(buf.chunk(9), chunk => new Mf1NestedArgs(chunk.subarray(0, 4), chunk.subarray(4, 8), chunk[8]))
+    return _.map(buf.chunk(9), chunk => new Mf1NestedArgs(
+      chunk.subarray(0, 4), // nt1
+      chunk.subarray(4, 8), // nt2
+      chunk[8], // par
+    ))
   }
 }
 
@@ -182,7 +199,15 @@ export class Mf1DetectionLog {
   nr: Buffer
   ar: Buffer
 
-  constructor (block: number, isKeyB: boolean, isNested: boolean, uid: Buffer, nt: Buffer, nr: Buffer, ar: Buffer) {
+  constructor (
+    block: number,
+    isKeyB: boolean,
+    isNested: boolean,
+    uid: Buffer,
+    nt: Buffer,
+    nr: Buffer,
+    ar: Buffer
+  ) {
     this.block = block
     this.isKeyB = isKeyB
     this.isNested = isNested
@@ -196,13 +221,13 @@ export class Mf1DetectionLog {
     if (!Buffer.isBuffer(buf) || buf.length < 18) throw new TypeError('buf should be a Buffer with length 18')
     const flag = buf.subarray(1, 2)
     return new Mf1DetectionLog(
-      buf[0],
-      flag.readBitLSB(0) === 1,
-      flag.readBitLSB(1) === 1,
-      buf.subarray(2, 6),
-      buf.subarray(6, 10),
-      buf.subarray(10, 14),
-      buf.subarray(14, 18),
+      buf[0], // block
+      flag.readBitLSB(0) === 1, // isKeyB
+      flag.readBitLSB(1) === 1, // isNested
+      buf.subarray(2, 6), // uid
+      buf.subarray(6, 10), // nt
+      buf.subarray(10, 14), // nr
+      buf.subarray(14, 18), // ar
     )
   }
 
@@ -230,11 +255,11 @@ export class Mf1EmuSettings {
   static fromCmd4009 (buf: Buffer): Mf1EmuSettings {
     if (!Buffer.isBuffer(buf) || buf.length !== 5) throw new TypeError('buf should be a Buffer with length 5')
     return new Mf1EmuSettings(
-      buf[0] === 1,
-      buf[1] === 1,
-      buf[2] === 1,
-      buf[3] === 1,
-      buf[4],
+      buf[0] === 1, // detection
+      buf[1] === 1, // gen1a
+      buf[2] === 1, // gen2
+      buf[3] === 1, // blockAntiColl
+      buf[4], // write
     )
   }
 }
